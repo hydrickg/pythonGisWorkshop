e87ec1f78865bbcc2eddf4d300cf1c54225f462b
Aaron Ruesch <asruesch@gmail.com>
2015-02-15 16:24:41 -0600

    more debugging

--- workshopEnv.py
+++ workshopEnv.py
@@ -7,22 +7,22 @@ from arcpy import env
 url = 'https://dl.dropboxusercontent.com/u/17521862/WLIA_2015_classdata.zip'
 home = os.path.expanduser("~")
 print home
-wd = home + '/pythonWorkshopWLIA2014'
+wd = home + '\\pythonWorkshopWLIA2014'
 print wd
-fgdb = wd + '/WLIA_pythonclass_data.gdb'
+fgdb = wd + '\\WLIA_pythonclass_data.gdb'
 print fgdb
 env.workspace = fgdb
 
 def refresh_data():
 	if not os.path.exists(wd):
 		os.makedirs(wd)
-	urllib.urlretrieve(url, wd + '/data.zip')
-	f = open(wd + '/data.zip', 'rb')
+	urllib.urlretrieve(url, wd + '\\data.zip')
+	f = open(wd + '\\data.zip', 'rb')
 	z = zipfile.ZipFile(f)
 	for name in z.namelist():
 		z.extract(name, wd)
 	f.close()
-	os.remove(wd + '/data.zip')
+	os.remove(wd + '\\data.zip')
 
 refresh_data()
 
